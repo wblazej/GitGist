cbbcf5d48778616023a254676f13171b2f802d51
wblazej <blazej.wrzosok@gmail.com>
2021-06-20 19:13:41 +0200

    Added languages icons support

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import DefaultIcon from './../img/langIcons/Default';
 import PythonIcon from './../img/langIcons/Python';
 import JavaScriptIcon from './../img/langIcons/JavaScript';
+import HTMLIcon from './../img/langIcons/HTML';
+import CSSIcon from './../img/langIcons/CSS';
+import TypeScriptIcon from './../img/langIcons/TypeScript';
 
 interface IProps {
     lang: string;
@@ -13,8 +17,14 @@ const Languages: React.FunctionComponent<IProps> = (props: IProps) => {
             return <PythonIcon/>
         case "JavaScript":
             return <JavaScriptIcon/>
+        case "HTML":
+            return <HTMLIcon/>
+        case "CSS":
+            return <CSSIcon/>
+        case "TypeScript":
+            return <TypeScriptIcon/>
         default:
-            return <PythonIcon/>
+            return <DefaultIcon/>
     }
 }
 
---
+++ src/img/langIcons/Default.tsx
@@ -0,0 +1,6 @@
+const Default = () =>
+    <svg width="128" height="128" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M74 14H34C28.4922 14 24 18.4922 24 24V104C24 109.508 28.4922 114 34 114H94C99.5078 114 104 109.508 104 104V44L74 14ZM96.5 49H69V21.5L96.5 49Z" fill="white"/>
+    </svg>
+
+export default Default;
